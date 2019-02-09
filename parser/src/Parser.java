24767015f5569aebfb68425deadd0c62cc3928ac
jzhu2020 <jzhu2020@k12.andoverma.us>
2019-02-09 09:39:43 -0500

    Add Parser.java which currently reads the .csv file into an ArrayList

---
+++ parser/src/Parser.java
@@ -0,0 +1,94 @@
+/**
+ * Reads US-Census-2017.csv and calls a Python script to display relevant information about it
+ * @author Justin Zhu
+ * @version 1.0 2/9/19
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Parser{
+
+    private static String[] columns = {"ID1", "ID2", "Name", "Census",
+            "2010Apr", "2010Jun", "2011", "2012", "2013", "2014", "2015", "2016", "2017"}; // 2010-2017 are estimates
+    private static int skipLines = 2;
+
+    private Scanner input;
+    private List<Territory> censusData;
+
+    public Parser(String fileName)  throws IOException {
+        input = new Scanner(new File(fileName));
+        parseFile();
+    }
+
+    public static String[] getFormattedLine(String row) {
+        return row.split(",");
+    }
+
+    private void parseFile() {
+        censusData = new ArrayList<Territory>();
+        int count = 0;
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            if(count++ < skipLines)
+                continue;
+            if(getFormattedLine(line).length == 0)
+                continue;
+            censusData.add(new Territory(line));
+        }
+    }
+
+    public String toString() {
+        String result = "";
+        for(int i = 0; i < censusData.size(); i++) {
+            result += censusData.get(i).toString() + "\n";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException{
+        Parser parser = new Parser("US-Census-2017.csv");
+        System.out.println(parser);
+    }
+
+    private class Territory {
+
+        private List<Object> data;
+
+        public Territory(String row) {
+            this(Parser.getFormattedLine(row));
+        }
+
+        public Territory(String[] row) {
+            data = new ArrayList<>();
+            for(int i = 0; i < row.length; i++) {
+                Object obj = row[i];
+                try {
+                    obj = Integer.parseInt(row[i]);
+                }
+                catch(Exception e) {
+                    // do nothing
+                }
+                data.add(obj);
+            }
+            if(data.size() != Parser.columns.length) {
+                throw new IllegalArgumentException("Input row length does not match the expected length");
+            }
+        }
+
+        public List<Object> getData() {
+            return data;
+        }
+
+        public String toString() {
+            String result = "";
+            for(int i = 0; i < Parser.columns.length; i++) {
+                result += Parser.columns[i] + ": " + data.get(i) + "\n";
+            }
+            return result;
+        }
+    }
+
+}
+
